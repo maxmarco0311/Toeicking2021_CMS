22712d3885c7dfbfcc7e9716e592c18feb461791
Max <maxmarco0311@gmail.com>
2021-03-24 09:56:16 +0800

    add SentenceIput class

---
+++ Toeicking2021/Models/SentenceInput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Toeicking2021.Models
+{
+    public class SentenceInput
+    {
+    }
+}
